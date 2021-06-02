f0f8578fcb9d0751778a6af5808843ea1d9a1a87
Anthony-Tag <anthony.escalante@revature.net>
2021-06-02 15:22:56 -0400

    table test

---
+++ src/app/table-test/table-test.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { TableTestDataSource, TableTestItem } from './table-test-datasource';
+
+@Component({
+  selector: 'app-table-test',
+  templateUrl: './table-test.component.html',
+  styleUrls: ['./table-test.component.css']
+})
+export class TableTestComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<TableTestItem>;
+  dataSource: TableTestDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  constructor() {
+    this.dataSource = new TableTestDataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}

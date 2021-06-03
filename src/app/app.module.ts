0bf8238dac39beb2fb2be09286c20e017f5389ba
Kenneth <kmarshall_pvamu@yahoo.com>
2021-06-03 15:59:31 -0500

    6321 end of day update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatGridListModule} from '@angular/material/grid-list';
-import { Test2Component } from './test2/test2.component';
-import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
-import { HeaderComponent } from './header/header.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { NewInitiativeFormComponent } from './new-initiative-form/new-initiative-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { AllInitiativeComponent } from './all-initiative/all-initiative.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LinksComponent } from './links/links.component';
+import { Test2Component } from './test2/test2.component';
+import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './header/header.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
-    Test2Component,
-    HeaderComponent,
+    NewInitiativeFormComponent,
     NavBarComponent,
     AllInitiativeComponent,
     LinksComponent
@@ -47,13 +51,24 @@ import { LinksComponent } from './links/links.component';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    TestComponent,
+    Test2Component,
+    HeaderComponent,
+
+  
   ],
   providers: [],
   bootstrap: [AppComponent]

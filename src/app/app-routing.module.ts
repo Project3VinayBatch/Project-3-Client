6c34d14aab9b23f92e6f284396a75301edddee50
Anthony-Tag <anthony.escalante@revature.net>
2021-06-03 16:20:40 -0400

    router and navbar complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllInitiativeComponent } from './all-initiative/all-initiative.component';
+import { NewInitiativeFormComponent } from './new-initiative-form/new-initiative-form.component';
 
 const routes: Routes = [
-  
+  {path: 'all-initiative', component:AllInitiativeComponent},
+  {path: 'new-initiative', component:NewInitiativeFormComponent}
 ];
 
 @NgModule({

222a8bd87cd1d0976f416c1f52023fb4c2b93499
aadilowrevature <abeil.adilow@revature.net>
2021-06-08 17:21:26 -0400

    added route test in progress

--- src/app/nav-bar/nav-bar.component.spec.ts
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -8,6 +8,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { NavBarComponent } from './nav-bar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
@@ -24,6 +27,7 @@ describe('NavBarComponent', () => {
         MatListModule,
         MatSidenavModule,
         MatToolbarModule,
+        RouterTestingModule
       ]
     }).compileComponents();
   }));
@@ -37,4 +41,27 @@ describe('NavBarComponent', () => {
   it('should compile', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should logout', () => {
+    const fixture = TestBed.createComponent(NavBarComponent);
+
+    const btn = fixture.debugElement.nativeElement.querySelector('#logout-btn');
+    expect(btn.routerLink).toEqual(!'/new-initiative')
+  });
+
+  it('should login', () => {
+    const fixture = TestBed.createComponent(NavBarComponent);
+
+    const btn = fixture.debugElement.nativeElement.querySelector('#login-btn');
+    //expect(btn.routerLink).toEqual("/sign-in")
+    const router = TestBed.inject(Router); //Get the router from the TestBed.
+    // @ts-ignore: force this private property value for testing.
+    router.url='/sign-in';
+
+    const spy = spyOn(router, 'navigate'); //Register a Spy on the router navigate function
+    expect(spy).toHaveBeenCalledWith(['/sign-in']); //Check if the router has been called with 'login'
+  
+
+  });
+
 });

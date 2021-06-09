d0d1efbf8ed6a208bfa71e9cff749aa75ef2a521
Anthony-Tag <anthony.escalante@revature.net>
2021-06-09 12:49:11 -0400

    icon and service added

--- src/app/initiative.service.ts
+++ src/app/initiative.service.ts
@@ -8,9 +8,14 @@ import { InitiativeDTO } from './model/initiativeDTO';
 })
 export class InitiativeService {
   private initiativePostUrl = 'http://localhost:8080/initiative';
+  private initiativesGetUrl = 'http://localhost:8080/initiatives'
   constructor(private http: HttpClient) {}
 
   postInitiative(initiativeDTO: InitiativeDTO): Observable<InitiativeDTO> {
     return this.http.post<InitiativeDTO>(this.initiativePostUrl, initiativeDTO);
+  }
+
+  getInitiatives(): Observable<InitiativeDTO[]> {
+    return this.http.get<InitiativeDTO[]>(this.initiativesGetUrl);
   }
 }

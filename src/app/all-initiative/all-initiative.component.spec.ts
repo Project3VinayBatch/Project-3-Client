9b3064fd3f24d1e8c6426e9bea2385597c722390
aadilowrevature <abeil.adilow@revature.net>
2021-06-13 16:39:22 -0400

    more tests

--- src/app/all-initiative/all-initiative.component.spec.ts
+++ src/app/all-initiative/all-initiative.component.spec.ts
@@ -7,10 +7,38 @@ import { RouterTestingModule } from "@angular/router/testing";
 
 import { AllInitiativeComponent } from './all-initiative.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Initiative } from '../model/initiative';
+import { User } from '../model/user';
+import { AuthService } from '../services/auth.service';
+
 
 describe('AllInitiativeComponent', () => {
   let component: AllInitiativeComponent;
   let fixture: ComponentFixture<AllInitiativeComponent>;
+  let routerSpy = {navigate: jasmine.createSpy('navigate')};
+  let authSrvc:AuthService;
+
+  
+  let fakeMembers: User[]=[{
+    "id":333,
+    "username":"Dog123",
+    "role":"Admin",
+    "password":null,
+    "initiatives":null,
+    "files":null
+  }];
+
+  let dummyData: Initiative[]=[{
+    "createdBy": 333,
+    "description": "This is not a real initiative. This does not exist",
+    "members": fakeMembers,
+    "pointOfContactId": 777,
+    "state": 0,
+    "title": "Test One"
+  }];
+
+
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -20,12 +48,15 @@ describe('AllInitiativeComponent', () => {
         MatPaginatorModule,
         MatSortModule,
         MatTableModule,
-        RouterTestingModule,
         HttpClientModule
         
       ],
       providers: [ 
-        HttpClientModule
+        HttpClientModule,         
+        
+        { provide: Router, useValue: routerSpy },
+        {provide: ActivatedRoute, useValue: routerSpy}
+
       ]
     }).compileComponents();
   }));
@@ -34,9 +65,38 @@ describe('AllInitiativeComponent', () => {
     fixture = TestBed.createComponent(AllInitiativeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    authSrvc=TestBed.inject(AuthService);
+
+    authSrvc.updateToken("Fake Login");
+
+
   });
 
   it('should compile', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should openModal',()=>{
+    component.openModal();
+  
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['new-initiative']);
+
+  });
+
+  it('should get record',()=>{
+    component.getRecord(0);
+  
+    expect(component.getRecord(0)).toBeFalsy();
+
+  });
+
+  it('should fill intiative list',()=>{
+    
+    component.fill(dummyData);   
+    expect(component.dataSource.initiativeList).toBe(dummyData);
+
+  });
+
+
+
 });
--- src/app/callback/callback.component.spec.ts
+++ src/app/callback/callback.component.spec.ts
@@ -1,12 +1,19 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../services/auth.service';
 
 import { CallbackComponent } from './callback.component';
 
 describe('CallbackComponent', () => {
   let component: CallbackComponent;
   let fixture: ComponentFixture<CallbackComponent>;
+    let routerSpy = {navigate: jasmine.createSpy('navigate')};
+    let authSrvc: AuthService;
+
+
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,19 +23,42 @@ describe('CallbackComponent', () => {
         HttpClientModule
       ],
       providers: [ 
-        HttpClientModule
+        HttpClientModule,
+        
+
+
       ]
     })
     .compileComponents();
+
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CallbackComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    authSrvc=TestBed.inject(AuthService);
+
+    
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+describe('logged in',()=>{
+  it('should redirect onInit',()=>{
+    authSrvc.updateToken("Fake Login");
+    //expect(routerSpy.navigate).toHaveBeenCalledWith(['all-initiative']);
+  });
+});
+
+describe('logged out',()=>{
+  it('should not route if logged out...',()=>{
+
+    authSrvc.removeToken();
+    expect(routerSpy.navigate).not.toHaveBeenCalledWith(['all-initiative']);
+  });
+
+});
+
 });
